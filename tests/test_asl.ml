e3c194f9c6083a5833d1c354d945791ee33d899c
Alastair Reid <alastair.reid@intel.com>
2022-05-17 18:21:26 -0700

    Tests: improve test framework to optionally check prettyprinting

--- tests/test_asl.ml
+++ tests/test_asl.ml
@@ -11,20 +11,24 @@ module TC  = Tcheck
 module AST = Asl_ast
 
 let format_value f v = Format.fprintf f "%s" (Value.pp_value v)
+let format_expr (e : AST.expr) : string = Utils.to_string2 (fun fmt -> Asl_fmt.expr fmt e)
 let value = Alcotest.testable format_value ( = )
 
 (* test that checks that an expression lexes, parses and typechecks *)
-let check_expr_tcheck tcenv what input _ : unit =
+let check_expr_tcheck (tcenv : TC.Env.t) (test_fmt : bool) (what : string) (input : string) _ : unit =
     LoadASL.report_type_error (fun _ -> Alcotest.fail "type error") (fun _ ->
         LoadASL.report_parse_error (fun _ -> Alcotest.fail "parse error") (fun _ ->
             let loc = AST.Unknown in
-            ignore (LoadASL.read_expr tcenv loc input)
+            let e = LoadASL.read_expr tcenv loc input in
+            if test_fmt then Alcotest.(check string) ("format "^what) input (format_expr e)
         )
     )
 
-(* simple test of static semantics: parsing and typechecking of correct expressions *)
-let test_static tcenv (name : string) (expr : string) : unit Alcotest.test_case =
-    (name, `Quick, check_expr_tcheck tcenv name expr)
+(* simple test of static semantics: parsing and typechecking of correct expressions
+ * and optionally checks that pretty-printing the AST produces exactly the input string
+ *)
+let test_static tcenv (test_fmt : bool) (name : string) (expr : string) : unit Alcotest.test_case =
+    (name, `Quick, check_expr_tcheck tcenv test_fmt name expr)
 
 let check_int what l r = Alcotest.check value what l (Value.VInt (Z.of_int r))
 
@@ -42,10 +46,10 @@ let tests : unit Alcotest.test_case list =
     let tcenv   = TC.Env.mkEnv TC.env0 in
     let env     = Eval.build_evaluation_environment prelude in
     [
-        (test_static tcenv "literals (int)" "1234");
-        (test_static tcenv "literals (real)" "10.0");
-        (test_static tcenv "literals (bits)" "'1111 0000'");
-        (test_static tcenv "UNKNOWN" "bits(4) UNKNOWN");
+        (test_static tcenv true "literals (int)" "1234");
+        (test_static tcenv true "literals (real)" "10.0");
+        (test_static tcenv true "literals (bits)" "'1111 0000'");
+        (test_static tcenv true "UNKNOWN" "bits(4) UNKNOWN");
         ("arith", `Quick, test_arith tcenv env)
     ]
 
