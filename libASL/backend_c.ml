b8814b1cf1b0189a3514832997b72144d23c9b9c
Nikolay Kosarev <nikolay.kosarev@intel.com>
2022-05-19 21:37:18 +0200

    Add C delimiters

--- libASL/backend_c.ml
+++ libASL/backend_c.ml
@@ -14,6 +14,60 @@ open Format_utils
 
 exception Unimplemented of (AST.l * string * (Format.formatter -> unit))
 
+let delimiter (fmt : PP.formatter) (s : string) : unit =
+  PP.pp_print_string fmt s
+
+(* C delimiters *)
+
+let amp                 (fmt : PP.formatter) : unit = delimiter fmt "&"
+let amp_amp             (fmt : PP.formatter) : unit = delimiter fmt "&&"
+let amp_eq              (fmt : PP.formatter) : unit = delimiter fmt "&="
+let bang                (fmt : PP.formatter) : unit = delimiter fmt "!"
+let bang_eq             (fmt : PP.formatter) : unit = delimiter fmt "!="
+let bar                 (fmt : PP.formatter) : unit = delimiter fmt "|"
+let bar_bar             (fmt : PP.formatter) : unit = delimiter fmt "||"
+let bar_eq              (fmt : PP.formatter) : unit = delimiter fmt "|="
+let caret               (fmt : PP.formatter) : unit = delimiter fmt "^"
+let caret_eq            (fmt : PP.formatter) : unit = delimiter fmt "^="
+let colon               (fmt : PP.formatter) : unit = delimiter fmt ":"
+let comma               (fmt : PP.formatter) : unit = delimiter fmt ","
+let dot                 (fmt : PP.formatter) : unit = delimiter fmt "."
+let dot_dot_dot         (fmt : PP.formatter) : unit = delimiter fmt "..."
+let eq                  (fmt : PP.formatter) : unit = delimiter fmt "="
+let eq_eq               (fmt : PP.formatter) : unit = delimiter fmt "=="
+let gt                  (fmt : PP.formatter) : unit = delimiter fmt ">"
+let gt_eq               (fmt : PP.formatter) : unit = delimiter fmt ">="
+let gt_gt               (fmt : PP.formatter) : unit = delimiter fmt ">>"
+let gt_gt_eq            (fmt : PP.formatter) : unit = delimiter fmt ">>="
+let hash                (fmt : PP.formatter) : unit = delimiter fmt "#"
+let hash_hash           (fmt : PP.formatter) : unit = delimiter fmt "##"
+let lbrace              (fmt : PP.formatter) : unit = delimiter fmt "{"
+let lbrack              (fmt : PP.formatter) : unit = delimiter fmt "["
+let lparen              (fmt : PP.formatter) : unit = delimiter fmt "("
+let lt                  (fmt : PP.formatter) : unit = delimiter fmt "<"
+let lt_eq               (fmt : PP.formatter) : unit = delimiter fmt "<="
+let lt_lt               (fmt : PP.formatter) : unit = delimiter fmt "<<"
+let lt_lt_eq            (fmt : PP.formatter) : unit = delimiter fmt "<<="
+let minus               (fmt : PP.formatter) : unit = delimiter fmt "-"
+let minus_eq            (fmt : PP.formatter) : unit = delimiter fmt "-="
+let minus_minus         (fmt : PP.formatter) : unit = delimiter fmt "--"
+let minus_qt            (fmt : PP.formatter) : unit = delimiter fmt "->"
+let percent             (fmt : PP.formatter) : unit = delimiter fmt "%"
+let percent_eq          (fmt : PP.formatter) : unit = delimiter fmt "%="
+let plus                (fmt : PP.formatter) : unit = delimiter fmt "+"
+let plus_eq             (fmt : PP.formatter) : unit = delimiter fmt "+="
+let plus_plus           (fmt : PP.formatter) : unit = delimiter fmt "++"
+let qmark               (fmt : PP.formatter) : unit = delimiter fmt "?"
+let rbrace              (fmt : PP.formatter) : unit = delimiter fmt "}"
+let rbrack              (fmt : PP.formatter) : unit = delimiter fmt "]"
+let rparen              (fmt : PP.formatter) : unit = delimiter fmt ")"
+let semicolon           (fmt : PP.formatter) : unit = delimiter fmt ";"
+let slash               (fmt : PP.formatter) : unit = delimiter fmt "/"
+let slash_eq            (fmt : PP.formatter) : unit = delimiter fmt "/="
+let star                (fmt : PP.formatter) : unit = delimiter fmt "*"
+let star_eq             (fmt : PP.formatter) : unit = delimiter fmt "*="
+let tilde               (fmt : PP.formatter) : unit = delimiter fmt "~"
+
 let declaration (fmt : PP.formatter) (x : AST.declaration) : unit =
   vbox fmt (fun _ ->
       match x with
