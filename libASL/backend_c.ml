30433865f7c0add0619d6fb732df36ed5b44976d
Nikolay Kosarev <nikolay.kosarev@intel.com>
2022-05-19 22:04:04 +0200

    Generate functions and procedures definitions
    Do not generate parameters and body yet.
    For now use int64_t type for representing the ASL's integer.

--- libASL/backend_c.ml
+++ libASL/backend_c.ml
@@ -14,10 +14,24 @@ open Format_utils
 
 exception Unimplemented of (AST.l * string * (Format.formatter -> unit))
 
+let mangle (s : string) : string =
+  (* TODO this should detect whether s is a reserved name in C and rename to avoid conflict *)
+  s
+
 let delimiter (fmt : PP.formatter) (s : string) : unit =
   PP.pp_print_string fmt s
 
 let keyword (fmt : PP.formatter) (s : string) : unit = PP.pp_print_string fmt s
+
+let ident_str (fmt : PP.formatter) (x : string) : unit =
+  PP.pp_print_string fmt x
+
+let ident (fmt : PP.formatter) (x : AST.ident) : unit =
+  match x with
+  | Ident s -> ident_str fmt (mangle s)
+  | FIdent (s, t) -> ident_str fmt (s ^ "_" ^ string_of_int t)
+
+let funname (fmt : PP.formatter) (x : AST.ident) : unit = ident fmt x
 
 (* C delimiters *)
 
@@ -118,9 +132,50 @@ let kw_uint32           (fmt : PP.formatter) : unit = keyword fmt "uint32_t"
 let kw_uint64           (fmt : PP.formatter) : unit = keyword fmt "uint64_t"
 let kw_uint8            (fmt : PP.formatter) : unit = keyword fmt "uint8_t"
 
+let braces (fmt : PP.formatter) (pp : unit -> unit) =
+  surround fmt lbrace rbrace pp
+
+let parens (fmt : PP.formatter) (pp : unit -> unit) =
+  surround fmt lparen rparen pp
+
+let ty (fmt : PP.formatter) (x : AST.ty) : unit =
+  match x with
+  (* TODO implement integer range analysis to determine the correct type width.
+   * For now use int64. *)
+  | Type_Integer _ -> kw_int64 fmt
+  | Type_App (_, _)
+  | Type_Array (_, _)
+  | Type_Bits _
+  | Type_Constructor _
+  | Type_OfExpr _
+  | Type_Register (_, _)
+  | Type_Tuple _ ->
+      raise (Unimplemented (AST.Unknown, "type", fun fmt -> FMTAST.ty fmt x))
+
+let function_header (fmt : PP.formatter) (ot : AST.ty option) (f : AST.ident)
+    (args : unit -> unit) : unit =
+  PP.pp_print_option ~none:(fun _ _ -> kw_void fmt) (fun _ t -> ty fmt t) fmt ot;
+  nbsp fmt;
+  funname fmt f;
+  parens fmt args
+
+let function_body (fmt : PP.formatter) : unit =
+  braces fmt (fun _ -> cut fmt);
+  cut fmt
+
 let declaration (fmt : PP.formatter) (x : AST.declaration) : unit =
   vbox fmt (fun _ ->
       match x with
+      | Decl_FunDefn (f, ps, args, t, b, loc) ->
+          function_header fmt (Some t) f (fun _ -> ());
+          nbsp fmt;
+          function_body fmt;
+          cut fmt
+      | Decl_ProcDefn (f, ps, args, b, loc) ->
+          function_header fmt None f (fun _ -> ());
+          nbsp fmt;
+          function_body fmt;
+          cut fmt
       | Decl_BuiltinFunction (f, ps, args, t, loc) -> ()
       | _ ->
           raise
--- tests/test_backend.ml
+++ tests/test_backend.ml
@@ -21,8 +21,39 @@ let test_builtin_fun decls () : unit =
              AST.Unknown );
        ])
 
+let test_fun_defn decls () : unit =
+  Alcotest.(check pass)
+    "no params, empty body" ()
+    (decls Format.std_formatter
+       [
+         AST.Decl_FunDefn
+           ( AST.Ident "name",
+             [],
+             [],
+             AST.Type_Integer None,
+             [],
+             AST.Unknown );
+       ])
+
+let test_proc_defn decls () : unit =
+  Alcotest.(check pass)
+    "no params, empty body" ()
+    (decls Format.std_formatter
+       [
+         AST.Decl_ProcDefn
+           ( AST.Ident "name",
+             [],
+             [],
+             [],
+             AST.Unknown );
+       ])
+
 let test_cases decls : unit Alcotest.test_case list =
-  [ ("built-in function", `Quick, test_builtin_fun decls) ]
+  [
+    ("built-in function", `Quick, test_builtin_fun decls);
+    ("function definition", `Quick, test_fun_defn decls);
+    ("procedure definition", `Quick, test_proc_defn decls);
+  ]
 
 let () =
   Alcotest.run "backend"
