086879dc08d64d96b7c381b7e5a6df7fd094ca03
Nikolay Kosarev <nikolay.kosarev@intel.com>
2022-05-19 22:17:27 +0200

    Add C keywords

--- libASL/backend_c.ml
+++ libASL/backend_c.ml
@@ -17,6 +17,8 @@ exception Unimplemented of (AST.l * string * (Format.formatter -> unit))
 let delimiter (fmt : PP.formatter) (s : string) : unit =
   PP.pp_print_string fmt s
 
+let keyword (fmt : PP.formatter) (s : string) : unit = PP.pp_print_string fmt s
+
 (* C delimiters *)
 
 let amp                 (fmt : PP.formatter) : unit = delimiter fmt "&"
@@ -67,6 +69,54 @@ let slash_eq            (fmt : PP.formatter) : unit = delimiter fmt "/="
 let star                (fmt : PP.formatter) : unit = delimiter fmt "*"
 let star_eq             (fmt : PP.formatter) : unit = delimiter fmt "*="
 let tilde               (fmt : PP.formatter) : unit = delimiter fmt "~"
+
+(* C keywords *)
+
+let kw_auto             (fmt : PP.formatter) : unit = keyword fmt "auto"
+let kw_break            (fmt : PP.formatter) : unit = keyword fmt "break"
+let kw_case             (fmt : PP.formatter) : unit = keyword fmt "case"
+let kw_char             (fmt : PP.formatter) : unit = keyword fmt "char"
+let kw_const            (fmt : PP.formatter) : unit = keyword fmt "const"
+let kw_continue         (fmt : PP.formatter) : unit = keyword fmt "continue"
+let kw_default          (fmt : PP.formatter) : unit = keyword fmt "default"
+let kw_do               (fmt : PP.formatter) : unit = keyword fmt "do"
+let kw_double           (fmt : PP.formatter) : unit = keyword fmt "double"
+let kw_else             (fmt : PP.formatter) : unit = keyword fmt "else"
+let kw_enum             (fmt : PP.formatter) : unit = keyword fmt "enum"
+let kw_extern           (fmt : PP.formatter) : unit = keyword fmt "extern"
+let kw_float            (fmt : PP.formatter) : unit = keyword fmt "float"
+let kw_for              (fmt : PP.formatter) : unit = keyword fmt "for"
+let kw_goto             (fmt : PP.formatter) : unit = keyword fmt "goto"
+let kw_if               (fmt : PP.formatter) : unit = keyword fmt "if"
+let kw_inline           (fmt : PP.formatter) : unit = keyword fmt "inline"
+let kw_int              (fmt : PP.formatter) : unit = keyword fmt "int"
+let kw_long             (fmt : PP.formatter) : unit = keyword fmt "long"
+let kw_register         (fmt : PP.formatter) : unit = keyword fmt "register"
+let kw_restrict         (fmt : PP.formatter) : unit = keyword fmt "restrict"
+let kw_return           (fmt : PP.formatter) : unit = keyword fmt "return"
+let kw_short            (fmt : PP.formatter) : unit = keyword fmt "short"
+let kw_signed           (fmt : PP.formatter) : unit = keyword fmt "signed"
+let kw_sizeof           (fmt : PP.formatter) : unit = keyword fmt "sizeof"
+let kw_static           (fmt : PP.formatter) : unit = keyword fmt "static"
+let kw_struct           (fmt : PP.formatter) : unit = keyword fmt "struct"
+let kw_switch           (fmt : PP.formatter) : unit = keyword fmt "switch"
+let kw_typedef          (fmt : PP.formatter) : unit = keyword fmt "typedef"
+let kw_union            (fmt : PP.formatter) : unit = keyword fmt "union"
+let kw_unsigned         (fmt : PP.formatter) : unit = keyword fmt "unsigned"
+let kw_void             (fmt : PP.formatter) : unit = keyword fmt "void"
+let kw_volatile         (fmt : PP.formatter) : unit = keyword fmt "volatile"
+let kw_while            (fmt : PP.formatter) : unit = keyword fmt "while"
+
+(* C pseudo-keywords *)
+
+let kw_bool             (fmt : PP.formatter) : unit = keyword fmt "bool"
+let kw_false            (fmt : PP.formatter) : unit = keyword fmt "false"
+let kw_int64            (fmt : PP.formatter) : unit = keyword fmt "int64_t"
+let kw_true             (fmt : PP.formatter) : unit = keyword fmt "true"
+let kw_uint16           (fmt : PP.formatter) : unit = keyword fmt "uint16_t"
+let kw_uint32           (fmt : PP.formatter) : unit = keyword fmt "uint32_t"
+let kw_uint64           (fmt : PP.formatter) : unit = keyword fmt "uint64_t"
+let kw_uint8            (fmt : PP.formatter) : unit = keyword fmt "uint8_t"
 
 let declaration (fmt : PP.formatter) (x : AST.declaration) : unit =
   vbox fmt (fun _ ->
