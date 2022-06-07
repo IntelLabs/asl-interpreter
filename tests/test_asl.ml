240705640d667ffb4e3b2e56295eb3f912a7ce40
Alastair Reid <alastair.reid@intel.com>
2022-06-07 14:30:18 +0100

    Tests: add tests of parsing/typechecking of expressions

--- tests/test_asl.ml
+++ tests/test_asl.ml
@@ -13,6 +13,19 @@ module AST = Asl_ast
 let format_value f v = Format.fprintf f "%s" (Value.pp_value v)
 let value = Alcotest.testable format_value ( = )
 
+(* test that checks that an expression lexes, parses and typechecks *)
+let check_expr_tcheck tcenv what input _ : unit =
+    LoadASL.report_type_error (fun _ -> Alcotest.fail "type error") (fun _ ->
+        LoadASL.report_parse_error (fun _ -> Alcotest.fail "parse error") (fun _ ->
+            let loc = AST.Unknown in
+            ignore (LoadASL.read_expr tcenv loc input)
+        )
+    )
+
+(* simple test of static semantics: parsing and typechecking of correct expressions *)
+let test_static tcenv (name : string) (expr : string) : unit Alcotest.test_case =
+    (name, `Quick, check_expr_tcheck tcenv name expr)
+
 let check_int what l r = Alcotest.check value what l (Value.VInt (Z.of_int r))
 
 let eval tcenv env (input : string): Value.value =
@@ -29,6 +42,10 @@ let tests : unit Alcotest.test_case list =
     let tcenv   = TC.Env.mkEnv TC.env0 in
     let env     = Eval.build_evaluation_environment prelude in
     [
+        (test_static tcenv "literals (int)" "1234");
+        (test_static tcenv "literals (real)" "10.0");
+        (test_static tcenv "literals (bits)" "'1111 0000'");
+        (test_static tcenv "UNKNOWN" "bits(4) UNKNOWN");
         ("arith", `Quick, test_arith tcenv env)
     ]
 
