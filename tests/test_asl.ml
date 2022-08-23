f078277f2cf2f835c5bfc58cedb96e5ed20fea0b
Alastair Reid <alastair.reid@intel.com>
2022-08-23 14:02:24 +0100

    Tests: restructure evaluation tests
    No changes to what is tested.
    
    But each test can now take an optional list of function/type declarations
    and I flattened the structure a little.

--- tests/test_asl.ml
+++ tests/test_asl.ml
@@ -30,7 +30,7 @@ let check_expr_tcheck (tcenv : TC.Env.t) (test_fmt : bool) (what : string)
           if test_fmt then
             Alcotest.(check string) ("format " ^ what) input (format_expr e)))
 
-let extend_tcenv (globals : TC.GlobalEnv.t) (declarations : string) : TC.Env.t =
+let extend_tcenv (globals : TC.GlobalEnv.t) (declarations : string) : (TC.Env.t * AST.declaration list) =
   let globals = TC.GlobalEnv.clone globals in
   let tcenv = TC.Env.mkEnv globals in
   LoadASL.report_type_error
@@ -41,8 +41,14 @@ let extend_tcenv (globals : TC.GlobalEnv.t) (declarations : string) : TC.Env.t =
         (fun _ ->
           let lexbuf = Lexing.from_string declarations in
           let t = Asl_parser.declarations_start Lexer.token lexbuf in
-          ignore (TC.tc_declarations globals false t);
-          tcenv))
+          let ds = TC.tc_declarations globals false t in
+          (tcenv, ds)))
+
+let extend_env (globals : TC.GlobalEnv.t) (prelude : AST.declaration list) (decls : string)
+  : (TC.Env.t * Eval.Env.t) =
+  let (tcenv, ds) = extend_tcenv globals decls in
+  let env = Eval.build_evaluation_environment (List.append prelude ds) in
+  (tcenv, env)
 
 (* simple test of static semantics: parsing and typechecking of correct expressions
  * and optionally checks that pretty-printing the AST produces exactly the input string
@@ -50,60 +56,42 @@ let extend_tcenv (globals : TC.GlobalEnv.t) (declarations : string) : TC.Env.t =
  *)
 let test_static (tcenv : TC.GlobalEnv.t) (test_fmt : bool) (name : string)
     (decls : string) (expr : string) : unit Alcotest.test_case =
-  (name, `Quick, check_expr_tcheck (extend_tcenv tcenv decls) test_fmt name expr)
-
-let check_int what l r = Alcotest.check value what l (Value.VInt (Z.of_int r))
-let check_string what l r = Alcotest.check value what l (Value.VString r)
-let check_bool what l r = Alcotest.check value what l (Value.VBool r)
+  let (tcenv', _) = extend_tcenv tcenv decls in
+  (name, `Quick, check_expr_tcheck tcenv' test_fmt name expr)
 
 let eval tcenv env (input : string) : Value.value =
   let loc = AST.Unknown in
   let e = LoadASL.read_expr tcenv loc input in
   Eval.eval_expr loc env e
 
-let test_primop_boolean (globals : TC.GlobalEnv.t) (env : Eval.Env.t) () : unit
-    =
-  let tcenv = TC.Env.mkEnv globals in
-  check_bool "implies" (eval tcenv env "FALSE --> FALSE") true;
-  check_bool "implies" (eval tcenv env "FALSE --> TRUE") true;
-  check_bool "implies" (eval tcenv env "TRUE --> FALSE") false;
-  check_bool "implies" (eval tcenv env "TRUE --> TRUE") true;
-  check_bool "iff" (eval tcenv env "FALSE <-> FALSE") true;
-  check_bool "iff" (eval tcenv env "FALSE <-> TRUE") false;
-  check_bool "iff" (eval tcenv env "TRUE <-> FALSE") false;
-  check_bool "iff" (eval tcenv env "TRUE <-> TRUE") true;
-  ()
+let test_bool (globals : TC.GlobalEnv.t) (prelude : AST.declaration list) (decls : string)
+    (l : string) (r : bool) () : unit =
+  let (tcenv, env) = extend_env globals prelude decls in
+  let what = l ^ " == " ^ Bool.to_string r in
+  Alcotest.check value what (Value.VBool r) (eval tcenv env l)
 
-let test_primop_string (globals : TC.GlobalEnv.t) (env : Eval.Env.t) () : unit =
-  let tcenv = TC.Env.mkEnv globals in
-  check_string "cvt_bits_str(1, '0') == \"'0'\""
-    (eval tcenv env "cvt_bits_str(1, '0')")
-    "'0'"
+let test_int (globals : TC.GlobalEnv.t) (prelude : AST.declaration list) (decls : string)
+    (l : string) (r : int) () : unit =
+  let (tcenv, env) = extend_env globals prelude decls in
+  let what = l ^ " == " ^ string_of_int r in
+  Alcotest.check value what (Value.VInt (Z.of_int r)) (eval tcenv env l)
 
-let test_primop_integer (globals : TC.GlobalEnv.t) (env : Eval.Env.t) () : unit
-    =
-  let tcenv = TC.Env.mkEnv globals in
-  check_string "HexStr(15) == \"0xf\"" (eval tcenv env "HexStr(15)") "0xf";
-  check_int "1+1 == 2" (eval tcenv env "1+1") 2;
-  check_int "5 DIV 3 == 1" (eval tcenv env "5 DIV 3") 1
-
-let test_string (globals : TC.GlobalEnv.t) (env : Eval.Env.t)
+let test_string (globals : TC.GlobalEnv.t) (prelude : AST.declaration list) (decls : string)
     (l : string) (r : string) () : unit =
-  let tcenv = TC.Env.mkEnv globals in
+  let (tcenv, env) = extend_env globals prelude decls in
   let what = l ^ " == " ^ r in
-  Alcotest.check value what (eval tcenv env l) (Value.VString r)
+  Alcotest.check value what (Value.VString r) (eval tcenv env l)
 
-let test_bits (globals : TC.GlobalEnv.t) (env : Eval.Env.t)
+let test_bits (globals : TC.GlobalEnv.t) (prelude : AST.declaration list) (decls : string)
     (l : string) (r : string) () : unit =
-  let tcenv = TC.Env.mkEnv globals in
+  let (tcenv, env) = extend_env globals prelude decls in
   let what = l ^ " == " ^ r in
-  Alcotest.check value what (eval tcenv env l) (Value.from_bitsLit r)
+  Alcotest.check value what (Value.from_bitsLit r) (eval tcenv env l)
 
 let tests : unit Alcotest.test_case list =
   let paths = [ "../../.." ] in
   let prelude = LoadASL.read_file paths "prelude.asl" true false in
   let globals = TC.env0 in
-  let env = Eval.build_evaluation_environment prelude in
   [
     test_static globals true "literals (int)" "" "1234";
     test_static globals true "literals (real)" "" "10.0";
@@ -141,12 +129,21 @@ let tests : unit Alcotest.test_case list =
            let (r :: integer, s :: boolean) = (1, TRUE);
            let (t :: integer, _ :: boolean) = (1, TRUE);
        end" "1";
-    ("operators (boolean)", `Quick, test_primop_boolean globals env);
-    ("operators (string)",  `Quick, test_primop_string globals env);
-    ("operators (integer)", `Quick, test_primop_integer globals env);
-    ("prelude (mul_bits_int)", `Quick, test_bits globals env "'00111' * 3" "10101"); (* == 21 *)
-    ("prelude (DecStr)", `Quick, test_string globals env "DecStr('10101')" "21");
-    ("prelude (HexStr)", `Quick, test_string globals env "HexStr('10101')" "0x15");
+    ("operators (implies)",    `Quick, test_bool globals prelude "" "FALSE --> FALSE" true);
+    ("operators (implies)",    `Quick, test_bool globals prelude "" "FALSE --> TRUE" true);
+    ("operators (implies)",    `Quick, test_bool globals prelude "" "TRUE --> FALSE" false);
+    ("operators (implies)",    `Quick, test_bool globals prelude "" "TRUE --> TRUE" true);
+    ("operators (iff)",        `Quick, test_bool globals prelude "" "FALSE <-> FALSE" true);
+    ("operators (iff)",        `Quick, test_bool globals prelude "" "FALSE <-> TRUE" false);
+    ("operators (iff)",        `Quick, test_bool globals prelude "" "TRUE <-> FALSE" false);
+    ("operators (iff)",        `Quick, test_bool globals prelude "" "TRUE <-> TRUE" true);
+    ("prelude (cvt_bits_str)", `Quick, test_string globals prelude "" "cvt_bits_str(1, '0')" "'0'");
+    ("prelude (HexStr (int))", `Quick, test_string globals prelude "" "HexStr(15)" "0xf");
+    ("prelude (+ (int))",      `Quick, test_int globals prelude "" "1+1" 2);
+    ("prelude (DIV)",          `Quick, test_int globals prelude "" "5 DIV 3" 1);
+    ("prelude (mul_bits_int)", `Quick, test_bits globals prelude "" "'00111' * 3" "10101"); (* == 21 *)
+    ("prelude (DecStr)",       `Quick, test_string globals prelude "" "DecStr('10101')" "21");
+    ("prelude (HexStr)",       `Quick, test_string globals prelude "" "HexStr('10101')" "0x15");
   ]
 
 let () = Alcotest.run "libASL" [ ("asl", tests) ]
