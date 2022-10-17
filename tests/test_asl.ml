1e6cbe8f2dcaf0baac8a4f7bf10d41fd0a3c3208
Nikolay Kosarev <nikolay.kosarev@intel.com>
2022-10-17 14:03:07 +0200

    Test that array getter is not affected by xform_getset

--- tests/test_asl.ml
+++ tests/test_asl.ml
@@ -231,6 +231,11 @@ let tests : unit Alcotest.test_case list =
        func P() S = G + 1; end
        func T() => integer i = 1; P(); S = G + 1; return i; end"
       "T() == 3");
+    ("array getter transform", `Quick, test_xform globals prelude Xform_getset.xform_decls
+      "var a :: array [0..7] of bits(8);
+       getter G{N}[index :: integer] => bits(N) return a[index][0 +: N]; end
+       func T() => bits(2) a[0] = Ones(8); return G[0]; end"
+      "T() == '11'");
   ]
 
 let () = Alcotest.run "libASL" [ ("asl", tests) ]
