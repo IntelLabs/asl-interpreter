IntelLabs/asl-interpreter

commit ce1d8fac59eeb064aab37ae381692a1217394838

Move error handling out of loadASL
Also try to gather error handling in one place

bin/asli.ml
@@ -22,7 +22,7 @@ let opt_batchmode = ref false
 let opt_show_banner = ref true
 
 (* on error, optionally exit if in batchmode *)
-let error _ : unit =
+let error () : unit =
   if !opt_batchmode then exit 1
 
 let projects : string list ref = ref []
@@ -99,14 +99,7 @@ let rec process_command (tcenv : TC.Env.t) (cpu : Cpu.cpu) (fname : string)
         while true do
           cpu.step ()
         done
-      with
-      | Value.Throw (_, Primops.Exc_ExceptionTaken) ->
-        Printf.printf "Exception taken\n";
-        error ()
-      | Value.EvalError (loc, msg) ->
-        Printf.printf "  %s: Evaluation error: %s\n" (pp_loc loc) msg;
-        error ()
-      )
+      with e -> Error.print_exception e; error ())
   | ":step" :: args ->
       let n = match args with
         | [n] -> int_of_string n
@@ -116,14 +109,7 @@ let rec process_command (tcenv : TC.Env.t) (cpu : Cpu.cpu) (fname : string)
           for i = 1 to n do
             cpu.step ();
           done
-      with
-      | Value.Throw (_, Primops.Exc_ExceptionTaken) ->
-        Printf.printf "Exception taken\n";
-        error ()
-      | Value.EvalError (loc, msg) ->
-        Printf.printf "  %s: Evaluation error: %s\n" (pp_loc loc) msg;
-        error ()
-      )
+      with e -> Error.print_exception e; error ())
   | _ ->
       if ';' = String.get input (String.length input - 1) then
         let s = LoadASL.read_stmt tcenv input in
@@ -148,21 +134,11 @@ let rec repl (tcenv : TC.Env.t) (cpu : Cpu.cpu) : unit =
   | None -> ()
   | Some input ->
       LNoise.history_add input |> ignore;
-      (try
-         LoadASL.report_eval_error
-           (fun _ -> error ())
-           (fun _ ->
-             LoadASL.report_type_error
-               (fun _ -> error ())
-               (fun _ ->
-                 LoadASL.report_parse_error
-                   (fun _ -> error ())
-                   (fun _ -> process_command tcenv cpu "<stdin>" input)))
-       with exc ->
-         Printf.printf "  Error %s\n" (Printexc.to_string exc);
-         Printexc.print_backtrace stdout;
-         error ()
-      );
+      try
+        process_command tcenv cpu "<stdin>" input
+      with e ->
+        Error.print_exception e;
+        error ();
       repl tcenv cpu
 
 let options =
@@ -196,47 +172,44 @@ let _ =
 let main () =
   Ocolor_format.prettify_formatter Format.std_formatter;
   Ocolor_config.set_color_capability Ocolor_config.Color4;
-  let paths = Option.value (Sys.getenv_opt "ASL_PATH") ~default:"." in
-  let paths = String.split_on_char ':' paths in
+  let paths = Option.value (Sys.getenv_opt "ASL_PATH") ~default:"."
+    |> String.split_on_char ':' in
   if !opt_print_version then Printf.printf "%s\n" version
-  else (
+  else
     if !opt_show_banner then (
       List.iter print_endline banner;
       print_endline "\nType :? for help");
-    let t = LoadASL.read_file paths "prelude.asl" true !opt_verbose in
-    let ts =
-      List.map
-        (fun filename ->
-          if Utils.endswith filename ".spec" then
-            LoadASL.read_spec paths filename !opt_verbose
-          else if Utils.endswith filename ".asl" then
-            LoadASL.read_file paths filename false !opt_verbose
-          else failwith ("Unrecognized file suffix on " ^ filename))
-        !opt_filenames
-    in
+    try (
+      let t = LoadASL.read_file paths "prelude.asl" true !opt_verbose in
+      let ts =
+        List.map
+          (fun filename ->
+            if Utils.endswith filename ".spec" then
+              LoadASL.read_spec paths filename !opt_verbose
+            else if Utils.endswith filename ".asl" then
+              LoadASL.read_file paths filename false !opt_verbose
+            else failwith ("Unrecognized file suffix on " ^ filename))
+          !opt_filenames
+      in
 
-    if !opt_print_spec then (
-      FMT.comment_list := Lexer.get_comments ();
-      FMT.declarations Format.std_formatter t;
-      Format.pp_print_flush Format.std_formatter ());
+      if !opt_print_spec then (
+        FMT.comment_list := Lexer.get_comments ();
+        FMT.declarations Format.std_formatter t;
+        Format.pp_print_flush Format.std_formatter ());
 
-    if !opt_verbose then Printf.printf "Building evaluation environment\n";
-    let env =
-      try Eval.build_evaluation_environment (List.concat (t :: ts))
-      with Value.EvalError (loc, msg) ->
-        Printf.printf "  %s: Evaluation error: %s\n" (pp_loc loc) msg;
-        exit 1
-    in
-    if !opt_verbose then Printf.printf "Built evaluation environment\n";
+      if !opt_verbose then Printf.printf "Building evaluation environment\n";
+      let env = Eval.build_evaluation_environment (List.concat (t :: ts)) in
+      if !opt_verbose then Printf.printf "Built evaluation environment\n";
 
-    let tcenv = TC.Env.mkEnv TC.env0 in
-    let cpu = Cpu.mkCPU env in
+      let tcenv = TC.Env.mkEnv TC.env0 in
+      let cpu = Cpu.mkCPU env in
 
-    List.iter (load_project tcenv cpu) !projects;
+      List.iter (load_project tcenv cpu) !projects;
 
-    LNoise.history_load ~filename:"asl_history" |> ignore;
-    LNoise.history_set ~max_length:100 |> ignore;
-    repl tcenv cpu)
+      LNoise.history_load ~filename:"asl_history" |> ignore;
+      LNoise.history_set ~max_length:100 |> ignore;
+      repl tcenv cpu
+    ) with e -> Error.print_exception e; exit 1
 
 let _ = ignore (main ())
 

bin/asl2v.ml
@@ -11,7 +11,6 @@ open LibASL
 module AST = Asl_ast
 module CP = Xform_constprop
 module ASL_FMT = Asl_fmt
-module FMT_Utils = Format_utils
 
 type backend = Backend_C | Backend_Verilog
 
@@ -52,29 +51,30 @@ let _ =
 let main () =
   let paths = Option.value (Sys.getenv_opt "ASL_PATH") ~default:"." in
   let paths = String.split_on_char ':' paths in
-  let t = LoadASL.read_file paths "prelude.asl" true !opt_verbose in
-  let ts =
-    List.map
-      (fun filename ->
-        if Utils.endswith filename ".spec" then
-          LoadASL.read_spec paths filename !opt_verbose
-        else if Utils.endswith filename ".asl" then
-          LoadASL.read_file paths filename false !opt_verbose
-        else failwith ("Unrecognized file suffix on " ^ filename))
-      !opt_filenames
-  in
-  let roots = List.map (fun f -> AST.FIdent (f, 0)) !roots in
-
-  let ds = List.concat (t :: ts) in
-
-  let keeps = List.map (fun r -> AST.Ident r) !keeps in
-
-  if true then
-    Utils.to_file "tmp.init0.asl" (fun fmt -> ASL_FMT.declarations fmt ds);
-  let ds = Asl_utils.reachable_decls (List.append roots keeps) ds in
-  if ds = [] then failwith "Couldn't find any roots";
 
   try
+    let t = LoadASL.read_file paths "prelude.asl" true !opt_verbose in
+    let ts =
+      List.map
+        (fun filename ->
+          if Utils.endswith filename ".spec" then
+            LoadASL.read_spec paths filename !opt_verbose
+          else if Utils.endswith filename ".asl" then
+            LoadASL.read_file paths filename false !opt_verbose
+          else failwith ("Unrecognized file suffix on " ^ filename))
+        !opt_filenames
+    in
+    let roots = List.map (fun f -> AST.FIdent (f, 0)) !roots in
+
+    let ds = List.concat (t :: ts) in
+
+    let keeps = List.map (fun r -> AST.Ident r) !keeps in
+
+    if true then
+      Utils.to_file "tmp.init0.asl" (fun fmt -> ASL_FMT.declarations fmt ds);
+    let ds = Asl_utils.reachable_decls (List.append roots keeps) ds in
+    if ds = [] then failwith "Couldn't find any roots";
+
     if !output_file == "" then
       failwith "Output file not specified (use -o foo.v to specify)";
 
@@ -111,23 +111,7 @@ let main () =
             Format.pp_print_string fmt "/* verilator lint_off UNPACKED */";
             Format.pp_print_cut fmt ();
             Backend_verilog.declarations fmt ds)
-  with
-  | Backend_c.Unimplemented (loc, what, pp)
-  | Backend_verilog.Unimplemented (loc, what, pp) ->
-      let fmt = Format.std_formatter in
-      Format.pp_print_newline fmt ();
-      FMT_Utils.vbox fmt (fun _ ->
-          ASL_FMT.loc fmt loc;
-          Format.fprintf fmt ": Unimplemented %s:" what;
-          FMT_Utils.indented fmt (fun _ -> pp fmt);
-          FMT_Utils.cut fmt)
-  | Value.EvalError (loc, msg) ->
-      let fmt = Format.std_formatter in
-      Format.pp_print_newline fmt ();
-      FMT_Utils.vbox fmt (fun _ ->
-          ASL_FMT.loc fmt loc;
-          Format.fprintf fmt ": Evaluation error %s:" msg;
-          FMT_Utils.cut fmt)
+  with e -> Error.print_exception e; exit 1
 
 let _ = ignore (main ())
 

libASL/dune
@@ -25,7 +25,7 @@
  (flags
   (:standard -w -27 -cclib -lstdc++))
  (modules asl_ast asl_fmt asl_parser asl_utils asl_visitor backend_c
-   backend_verilog cpu elf eval flags format_utils lattice lexer lexersupport
+   backend_verilog cpu elf error eval flags format_utils lattice lexer lexersupport
    loadASL primops tcheck utils value visitor
    xform_casesplit xform_constprop xform_mono xform_tuples)
  (libraries ocolor pprint zarith z3))

libASL/error.ml
@@ -1,0 +1,50 @@
+(****************************************************************
+ * Error
+ *
+ * Copyright Intel Inc (c) 2021-2022
+ * SPDX-Licence-Identifier: BSD-3-Clause
+ ****************************************************************)
+open Asl_ast
+module TC = Tcheck
+module Parser = Asl_parser
+module FMT_Utils = Format_utils
+module ASL_FMT = Asl_fmt
+open Asl_utils
+
+let print_exception (e : exn) : unit =
+  match e with
+  | Parse_error_locn (l, s) ->
+      Printf.printf "  Syntax error %s at %s\n" s (pp_loc l);
+  | PrecedenceError (loc, op1, op2) ->
+      Printf.printf
+        "  Syntax error: operators %s and %s require parentheses to \
+         disambiguate expression at location %s\n"
+        (pp_binop op1) (pp_binop op2) (pp_loc loc);
+  | Parser.Error ->
+      Printf.printf "  Parser error\n";
+  | TC.UnknownObject (loc, what, x) ->
+    Printf.printf "  %s: Type error: Unknown %s %s\n" (pp_loc loc) what x
+  | TC.DoesNotMatch (loc, what, x, y) ->
+    Printf.printf "  %s: Type error: %s %s does not match %s\n" (pp_loc loc) what x y
+  | TC.IsNotA (loc, what, x) ->
+    Printf.printf "  %s: Type error: %s is not a %s\n" (pp_loc loc) x what
+  | TC.Ambiguous (loc, what, x) ->
+    Printf.printf "  %s: Type error: %s %s is ambiguous\n" (pp_loc loc) what x
+  | TC.TypeError (loc, what) ->
+    Printf.printf "  %s: Type error: %s\n" (pp_loc loc) what
+  | Value.EvalError (loc, msg) ->
+    Printf.printf "  %s: Evaluation error: %s\n" (pp_loc loc) msg
+  | Value.Throw (_, Primops.Exc_ExceptionTaken) ->
+    Printf.printf "Exception taken\n";
+  | Backend_c.Unimplemented (loc, what, pp)
+  | Backend_verilog.Unimplemented (loc, what, pp) ->
+      let fmt = Format.std_formatter in
+      Format.pp_print_newline fmt ();
+      FMT_Utils.vbox fmt (fun _ ->
+          ASL_FMT.loc fmt loc;
+          Format.fprintf fmt ": Unimplemented %s:" what;
+          FMT_Utils.indented fmt (fun _ -> pp fmt);
+          FMT_Utils.cut fmt)
+  | _ ->
+    Printf.printf "  Error %s\n" (Printexc.to_string e);
+    Printexc.print_backtrace stdout;

libASL/error.mli
@@ -1,0 +1,8 @@
+(****************************************************************
+ * Error
+ *
+ * Copyright Intel Inc (c) 2021-2022
+ * SPDX-Licence-Identifier: BSD-3-Clause
+ ****************************************************************)
+
+val print_exception : exn -> unit

libASL/loadASL.ml
@@ -14,46 +14,6 @@ module AST = Asl_ast
 open Lexing
 open Asl_utils
 
-let report_parse_error (on_error : unit -> 'a) (f : unit -> 'a) : 'a =
-  try f () with
-  | Parse_error_locn (l, s) ->
-      Printf.printf "  Syntax error %s at %s\n" s (pp_loc l);
-      on_error ()
-  | PrecedenceError (loc, op1, op2) ->
-      Printf.printf
-        "  Syntax error: operators %s and %s require parentheses to \
-         disambiguate expression at location %s\n"
-        (pp_binop op1) (pp_binop op2) (pp_loc loc);
-      on_error ()
-  | Parser.Error ->
-      Printf.printf "  Parser error\n";
-      on_error ()
-
-let report_type_error (on_error : unit -> 'a) (f : unit -> 'a) : 'a =
-  try f () with
-  | TC.UnknownObject (loc, what, x) ->
-      Printf.printf "  %s: Type error: Unknown %s %s\n" (pp_loc loc) what x;
-      on_error ()
-  | TC.DoesNotMatch (loc, what, x, y) ->
-      Printf.printf "  %s: Type error: %s %s does not match %s\n" (pp_loc loc)
-        what x y;
-      on_error ()
-  | TC.IsNotA (loc, what, x) ->
-      Printf.printf "  %s: Type error: %s is not a %s\n" (pp_loc loc) x what;
-      on_error ()
-  | TC.Ambiguous (loc, what, x) ->
-      Printf.printf "  %s: Type error: %s %s is ambiguous\n" (pp_loc loc) what x;
-      on_error ()
-  | TC.TypeError (loc, what) ->
-      Printf.printf "  %s: Type error: %s\n" (pp_loc loc) what;
-      on_error ()
-
-let report_eval_error (on_error : unit -> 'a) (f : unit -> 'a) : 'a =
-  try f ()
-  with Value.EvalError (loc, msg) ->
-    Printf.printf "  %s: Evaluation error: %s\n" (pp_loc loc) msg;
-    on_error ()
-
 let rec find_file (paths : string list) (filename : string) : string =
   match paths with
   | [] -> failwith ("Can't find file '" ^ filename ^ "' on path")
@@ -68,15 +28,14 @@ let parse_file (paths : string list) (filename : string) (isPrelude : bool)
   if verbose then Printf.printf "Processing %s\n" fname;
   let lexbuf = Lexing.from_channel inchan in
   lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = fname };
-  let t =
-    report_parse_error
-      (fun _ ->
-        print_endline (pp_loc (Range (lexbuf.lex_start_p, lexbuf.lex_curr_p)));
-        exit 1)
-      (fun _ ->
-        (* Run the parser on this line of input. *)
-        if verbose then Printf.printf "- Parsing %s\n" fname;
-        Parser.declarations_start Lexer.token lexbuf)
+  (* Run the parser on this line of input. *)
+  if verbose then
+    Printf.printf "- Parsing %s\n" fname;
+  let t = try
+    Parser.declarations_start Lexer.token lexbuf
+  with e ->
+    print_endline (pp_loc (Range (lexbuf.lex_start_p, lexbuf.lex_curr_p)));
+    raise e
   in
   close_in inchan;
   t
@@ -88,24 +47,21 @@ let read_file (paths : string list) (filename : string) (isPrelude : bool)
   if false then (
     FMT.comment_list := Lexer.get_comments ();
     FMT.declarations Format.std_formatter t;
-    Format.pp_print_flush Format.std_formatter ());
-  if verbose then
+    Format.pp_print_flush Format.std_formatter ()
+  );
+  if verbose then (
     Printf.printf "  - Got %d declarations from %s\n%!" (List.length t) filename;
+    Printf.printf "- Typechecking %s\n%!" filename;
+  );
 
-  let t' =
-    report_type_error
-      (fun _ -> exit 1)
-      (fun _ ->
-        if verbose then Printf.printf "- Typechecking %s\n%!" filename;
-        TC.tc_declarations TC.env0 isPrelude t)
-  in
+  let t' = TC.tc_declarations TC.env0 isPrelude t in
 
   if false then FMT.declarations Format.std_formatter t';
-  if verbose then
+  if verbose then (
     Printf.printf "  - Got %d typechecked declarations from %s\n%!"
       (List.length t') filename;
-
-  if verbose then Printf.printf "Finished %s\n%!" filename;
+    Printf.printf "Finished %s\n%!" filename;
+  );
   flush stdout;
   t'
 

libASL/loadASL.mli
@@ -8,10 +8,6 @@
 module AST = Asl_ast
 module TC = Tcheck
 
-val report_parse_error : (unit -> 'a) -> (unit -> 'a) -> 'a
-val report_type_error : (unit -> 'a) -> (unit -> 'a) -> 'a
-val report_eval_error : (unit -> 'a) -> (unit -> 'a) -> 'a
-
 val read_file :
   string list -> string -> bool -> bool -> Asl_ast.declaration list
 (** Parse and typecheck ASL file *)

tests/test_asl.ml
@@ -19,30 +19,18 @@ let value = Alcotest.testable format_value ( = )
 (* test that checks that an expression lexes, parses and typechecks *)
 let check_expr_tcheck (tcenv : TC.Env.t) (test_fmt : bool) (what : string)
     (input : string) _ : unit =
-  LoadASL.report_type_error
-    (fun _ -> Alcotest.fail "type error")
-    (fun _ ->
-      LoadASL.report_parse_error
-        (fun _ -> Alcotest.fail "parse error")
-        (fun _ ->
-          let loc = AST.Unknown in
-          let e = LoadASL.read_expr tcenv loc input in
-          if test_fmt then
-            Alcotest.(check string) ("format " ^ what) input (format_expr e)))
+  let loc = AST.Unknown in
+  let e = LoadASL.read_expr tcenv loc input in
+  if test_fmt then
+    Alcotest.(check string) ("format " ^ what) input (format_expr e)
 
 let extend_tcenv (globals : TC.GlobalEnv.t) (declarations : string) : (TC.Env.t * AST.declaration list) =
   let globals = TC.GlobalEnv.clone globals in
   let tcenv = TC.Env.mkEnv globals in
-  LoadASL.report_type_error
-    (fun _ -> Alcotest.fail "type error in decls")
-    (fun _ ->
-      LoadASL.report_parse_error
-        (fun _ -> Alcotest.fail "parse error in decls")
-        (fun _ ->
-          let lexbuf = Lexing.from_string declarations in
-          let t = Asl_parser.declarations_start Lexer.token lexbuf in
-          let ds = TC.tc_declarations globals false t in
-          (tcenv, ds)))
+  let lexbuf = Lexing.from_string declarations in
+  let t = Asl_parser.declarations_start Lexer.token lexbuf in
+  let ds = TC.tc_declarations globals false t in
+  (tcenv, ds)
 
 let extend_env (globals : TC.GlobalEnv.t) (prelude : AST.declaration list) (decls : string)
   : (TC.Env.t * Eval.Env.t) =

commit fc17f17cefbdd851c4946708e3b971f2cf4f3ab0

Fix bug where ASLi quit after first statement
Bug was introduced in ce1d8fac59eeb064aab37ae381692a1217394838

bin/asli.ml
@@ -134,11 +134,12 @@ let rec repl (tcenv : TC.Env.t) (cpu : Cpu.cpu) : unit =
   | None -> ()
   | Some input ->
       LNoise.history_add input |> ignore;
-      try
+      (try
         process_command tcenv cpu "<stdin>" input
       with e ->
         Error.print_exception e;
         error ();
+      );
       repl tcenv cpu
 
 let options =