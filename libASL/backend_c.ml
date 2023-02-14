920f8fc8d493cafb26555ffa24a521550d3822de
Alastair Reid <alastair.reid@intel.com>
2023-02-14 08:03:35 +0000

    C backend: broaden range of lexprs allowed in slice assignments
    In particular, allow any combination of
    field selection and array indexing
    
       x.f[...] = ...;
       x.f.g[...] = ...;
       A[i][...] = ...;
       A[i][j][...] = ...;

--- libASL/backend_c.ml
+++ libASL/backend_c.ml
@@ -782,10 +782,21 @@ let rec lexpr (loc : AST.l) (fmt : PP.formatter) (x : AST.lexpr) : unit =
         (Unimplemented
            (loc, "l-expression", fun fmt -> FMTAST.lexpr fmt x))
 
+let rec lexpr_to_expr (loc : AST.l) (x : AST.lexpr) : AST.expr =
+  ( match x with
+  | LExpr_Var v -> Expr_Var v
+  | LExpr_Field (l, f) -> Expr_Field (lexpr_to_expr loc l, f)
+  | LExpr_Array (l, e) -> Expr_Array (lexpr_to_expr loc l, e)
+  | _ ->
+      raise
+        (Unimplemented
+           (loc, "lexpr_to_expr", fun fmt -> FMTAST.lexpr fmt x))
+  )
+
 let lexpr_assign (loc : AST.l) (fmt : PP.formatter) (x : AST.lexpr) (r : AST.expr) : unit =
   match x with
-  | LExpr_Slices (_, LExpr_Var v, [ s ]) ->
-      lslice loc fmt r (Expr_Var v) s;
+  | LExpr_Slices (_, l, [ s ]) ->
+      lslice loc fmt r (lexpr_to_expr loc l) s;
       semicolon fmt
   | LExpr_Wildcard ->
       make_cast fmt (fun _ -> kw_void fmt) (fun _ -> expr loc fmt r);
