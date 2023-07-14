25bfc7305f362b4a51daa83d2e21b150e4c5d983
Alastair Reid <alastair.reid@intel.com>
2023-07-14 08:12:11 +0100

    Value: fix lsl/lsr/asr_bits support in interpreter (and constprop)

--- libASL/value.ml
+++ libASL/value.ml
@@ -503,11 +503,11 @@ let eval_prim (f : string) (tvs : value list) (vs : value list) : value option =
   | "eor_bits", [ VInt n ], [ VBits x; VBits y ] ->
       Some (VBits (prim_eor_bits x y))
   | "not_bits", [ VInt n ], [ VBits x ] -> Some (VBits (prim_not_bits x))
-  | "zeros_bits", [ VInt n ], [VInt x] -> Some (VBits (prim_zeros_bits x))
-  | "ones_bits", [ VInt n ], [VInt x] -> Some (VBits (prim_ones_bits x))
-  | "lsl_bits", [], [ VBits x; VInt d ] -> Some (VBits (prim_lsl x d))
-  | "lsr_bits", [], [ VBits x; VInt d ] -> Some (VBits (prim_lsr x d))
-  | "asr_bits", [], [ VBits x; VInt d ] -> Some (VBits (prim_asr x d))
+  | "zeros_bits", [ VInt n ], [_] -> Some (VBits (prim_zeros_bits n))
+  | "ones_bits", [ VInt n ], [_] -> Some (VBits (prim_ones_bits n))
+  | "lsl_bits", [_], [ VBits x; VInt d ] -> Some (VBits (prim_lsl x d))
+  | "lsr_bits", [_], [ VBits x; VInt d ] -> Some (VBits (prim_lsr x d))
+  | "asr_bits", [_], [ VBits x; VInt d ] -> Some (VBits (prim_asr x d))
   | "replicate_bits", [ _; _ ], [ VBits x; VInt y ] ->
       Some (VBits (prim_replicate_bits x y))
   | "zero_extend_bits", [ _; _ ], [ VBits x; VInt y ] ->
