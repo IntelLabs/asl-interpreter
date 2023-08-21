04385bf428369fc4220336d1803be439dcf8bb9b
Nikolay Kosarev <nikolay.kosarev@intel.com>
2023-08-21 14:52:04 +0200

    Add interface for desugaring transformation

---
+++ libASL/xform_desugar.mli
@@ -0,0 +1,10 @@
+(****************************************************************
+ * ASL desugaring transformations
+ *
+ * Copyright Intel Inc (c) 2023
+ * SPDX-Licence-Identifier: BSD-3-Clause
+ ****************************************************************)
+
+module AST = Asl_ast
+
+val xform_decls : AST.declaration list -> AST.declaration list
