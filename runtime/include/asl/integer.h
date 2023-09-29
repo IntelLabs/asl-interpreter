ac35d3bab3828c91197f93d1e9bcb01114c7fd26
Nikolay Kosarev <nikolay.kosarev@intel.com>
2023-09-29 20:47:04 +0200

    Remove ASL_[1024,512,256]int_t types
    as unsupported

--- runtime/include/asl/integer.h
+++ runtime/include/asl/integer.h
@@ -20,18 +20,6 @@ typedef struct {
         uint64_t u64[2];
 } ASL_int128_t;
 
-typedef struct {
-        uint64_t u64[4];
-} ASL_int256_t;
-
-typedef struct {
-        uint64_t u64[8];
-} ASL_int512_t;
-
-typedef struct {
-        uint64_t u64[16];
-} ASL_int1024_t;
-
 typedef ASL_int64_t ASL_int_t;
 
 #define ASL_int_zero(sizeof_x) \
@@ -68,72 +56,6 @@ static inline ASL_int128_t
 ASL_int_max_128()
 {
         return ASL_int_128(INT64_MAX, UINT64_MAX);
-}
-
-static inline ASL_int256_t
-ASL_int_256(uint64_t v3, uint64_t v2, uint64_t v1, uint64_t v0)
-{
-        return (ASL_int256_t){ { v0, v1, v2, v3 } };
-}
-
-static inline ASL_int256_t
-ASL_int_zero_256()
-{
-        return ASL_int_256(0, 0, 0, 0);
-}
-
-static inline ASL_int256_t
-ASL_int_max_256()
-{
-        return ASL_int_256(UINT64_MAX, UINT64_MAX,
-                           UINT64_MAX, UINT64_MAX);
-}
-
-static inline ASL_int512_t
-ASL_int_512(uint64_t v7, uint64_t v6, uint64_t v5, uint64_t v4,
-            uint64_t v3, uint64_t v2, uint64_t v1, uint64_t v0)
-{
-        return (ASL_int512_t){ { v0, v1, v2, v3, v4, v5, v6, v7 } };
-}
-
-static inline ASL_int512_t
-ASL_int_zero_512()
-{
-        return ASL_int_512(0, 0, 0, 0, 0, 0, 0, 0);
-}
-
-static inline ASL_int512_t
-ASL_int_max_512()
-{
-        return ASL_int_512(UINT64_MAX, UINT64_MAX,
-                           UINT64_MAX, UINT64_MAX,
-                           UINT64_MAX, UINT64_MAX,
-                           UINT64_MAX, UINT64_MAX);
-}
-
-static inline ASL_int1024_t
-ASL_int_1024(uint64_t v15, uint64_t v14, uint64_t v13, uint64_t v12,
-             uint64_t v11, uint64_t v10, uint64_t v9, uint64_t v8,
-             uint64_t v7, uint64_t v6, uint64_t v5, uint64_t v4,
-             uint64_t v3, uint64_t v2, uint64_t v1, uint64_t v0)
-{
-        return (ASL_int1024_t){ { v0, v1, v2, v3, v4, v5, v6, v7,
-                                  v8, v9, v10, v11, v12, v13, v14, v15 } };
-}
-
-static inline ASL_int1024_t
-ASL_int_zero_1024()
-{
-        return ASL_int_1024(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-}
-
-static inline ASL_int1024_t
-ASL_int_max_1024()
-{
-        return ASL_int_1024(UINT64_MAX, UINT64_MAX, UINT64_MAX, UINT64_MAX,
-                            UINT64_MAX, UINT64_MAX, UINT64_MAX, UINT64_MAX,
-                            UINT64_MAX, UINT64_MAX, UINT64_MAX, UINT64_MAX,
-                            UINT64_MAX, UINT64_MAX, UINT64_MAX, UINT64_MAX);
 }
 
 #ifdef __cplusplus
